829ff849144877f390a524129874ecc3040f26df
wanz <lzh60587@gmail.com>
2018-10-08 18:55:33 +0800

    an ugly tk drawing

---
+++ py/SimpleGraphics Drawing.py
@@ -0,0 +1,22 @@
+from SimpleGraphics import *
+
+background("gray")
+setColor("yellow")
+ellipse(150, 50, 500, 500)
+
+setColor("black")
+ellipse(275, 175, 75, 50)
+ellipse(450, 175, 75, 50)
+
+setColor("blue")
+line(260, 160, 360, 160, 360, 240, 260, 240, 260, 160, 215, 130)
+line(540, 160, 440, 160, 440, 240, 540, 240, 540, 160, 585, 130)
+line(360, 200, 440, 200)
+
+setOutline(255, 255, 255)
+setFill(255, 192, 192)
+blob(300, 300, 500, 300, 425, 400, 375, 400)
+
+setColor("coral1")
+pieSlice(312.5, 350, 175, 175, 180, 180)
+
